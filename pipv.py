f8eff93390b9a9895d1f20211b20047684d2f88d
boringstudent <boring_student@qq.com>
2025-01-18 19:17:41 +0800

    h

---
+++ pipv.py
@@ -0,0 +1,14 @@
+# 阿里源
+# pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pipv.py   # 下载安装脚本
+# sudo python get-pipv.py    # 运行安装脚本
+# 升级 pip
+# pip install -U ipaddress
+# 安装包
+# pip install PyQt6
+# 升级包
+# pip show ipaddress
+# 清华大学 https://pypi.tuna.tsinghua.edu.cn/simple/
+# 阿里云 http://mirrors.aliyun.com/pypi/simple/
+# 中国科技大学 https://pypi.mirrors.ustc.edu.cn/simple/
+# 中国科学技术大学 http://pypi.mirrors.ustc.edu.cn/simple/
