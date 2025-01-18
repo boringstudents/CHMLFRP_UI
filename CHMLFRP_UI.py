1dea35bfc96a41e47c7007a297cdd0c2e3f3207e
boringstudent <boring_student@qq.com>
2025-01-18 18:57:18 +0800

    h

--- CHMLFRP_UI.py
+++ CHMLFRP_UI.py
@@ -22,7 +22,6 @@ import requests
 from PyQt6.QtCore import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
-from PyQt6.QtNetwork import *
 from mcstatus import *
 from requests import *
 import psutil
