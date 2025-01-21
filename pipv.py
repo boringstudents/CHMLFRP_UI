9e283e3c04fdf1f23ac2fbf7fd57cb12bedf3bdd
boringstudent <boring_student@qq.com>
2025-01-22 01:55:17 +0800

    Token无效时的提示信息和停止后续基于token的操作
    和打包运行时exe的提权操作

--- pipv.py
+++ pipv.py
@@ -15,33 +15,3 @@
 # 阿里云 http://mirrors.aliyun.com/pypi/simple/
 # 中国科技大学 https://pypi.mirrors.ustc.edu.cn/simple/
 # 中国科学技术大学 http://pypi.mirrors.ustc.edu.cn/simple/
-
-# import win32security
-# import win32api
-# import win32con
-# import win32process
-#
-#
-# def elevate_privileges():
-#     try:
-#         current_process = win32api.GetCurrentProcess()
-#         token = win32security.OpenProcessToken(current_process, win32con.TOKEN_ADJUST_PRIVILEGES | win32con.TOKEN_QUERY)
-#
-#         privilege_id = win32security.LookupPrivilegeValue(None, win32con.SE_DEBUG_NAME)
-#
-#         new_privileges = [(privilege_id, win32con.SE_PRIVILEGE_ENABLED)]
-#         win32security.AdjustTokenPrivileges(token, False, new_privileges)
-#
-#         return True
-#     except Exception as e:
-#         print(f"提升权限时出错: {e}")
-#         return False
-#
-#
-# # 使用示例
-# if __name__ == "__main__":
-#     if elevate_privileges():
-#         print("成功提升权限")
-#         # 在这里添加需要管理员权限的代码
-#     else:
-#         print("提升权限失败")
